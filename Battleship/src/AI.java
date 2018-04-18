92f5141c162423c19b6ee87e7d03e199d2349517
YSanson <yvan.sanson@etu.umontpellier.fr>
2018-04-18 16:50:34 +0200

    Début du travail sur l'IA

--- Battleship/src/AI.java
+++ Battleship/src/AI.java
@@ -1,6 +1,28 @@
+import java.util.Random;
+
 public class AI extends Joueur {
 
     public AI() {
-        super("AI");
+        super("T-1000");
     }
+
+    @Override
+    public void initialize(){
+
+    }
+
+    public int sendMissile(Joueur adversary){
+        String missileCoord=randomCoord();
+        return super.sendMissile(missileCoord, adversary);
+    }
+
+    public String randomCoord(){
+        Random r = new Random();
+        String line="ABCDEFGHIJ";
+        char randLine=line.charAt(r.nextInt(line.length()));
+        int randColumn=r.nextInt(10)+1;
+        String randCoord = randLine+Integer.toString(randColumn);
+        return randCoord;
+    }
+
 }
